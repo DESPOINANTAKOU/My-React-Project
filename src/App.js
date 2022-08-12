b92d1c961c4348dfa70e165a89b19e7cb07b0abc
despoina <dntakou@upgrade.net.gr>
2022-08-12 21:04:35 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-     <Routes>
-        <Route path="/" element={<></>} />
-        <Route path="" element={<></>} />
-      </Routes>
-    </BrowserRouter>
     </div>
   );
 }
 export default App;
+
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import LoginForm from './components/LoginForm';
+import { Routes, Route, Link} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="loginform" element={<LoginForm />} />
+      <Route path="*" element={<>
+      <p>Wrong URL please return to the Home Page!</p>
+      </>} />
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
