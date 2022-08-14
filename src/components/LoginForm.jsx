39134aa47475184ad2a9c49514c00712795a78cf
despoina <dntakou@upgrade.net.gr>
2022-08-14 16:08:25 +0300

    4o commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function LoginForm () {
     return (
-        <></>
+        <>
+            <div>
+                <form>
+                    <input type='text' />
+                    <input type= "password" />
+                    <button type="submit" >Submit!</button>
+                </form>
+            </div>
+        </>
     )
 }
